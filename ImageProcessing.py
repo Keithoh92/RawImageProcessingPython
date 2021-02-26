f657ff98d81112d4d2f8a093753f775ef343c0fe
KeithDev2020 <eire19@hotmail.com>
2021-02-26 14:35:56 +0000

    Added Gausian filter proccessing and edge detection using convolution

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -4,7 +4,10 @@ import time
 from PIL import Image
 import random
 from matplotlib import pyplot as plt
+import math
 import queue
+import argparse
+from skimage.color import rgb2gray
 queue = queue.Queue(maxsize=0) 
 
 #defines the fore/back-ground colour of pixels depending on the threshold
@@ -317,7 +320,80 @@ def connected_component_labelling(img):
     return labels
 
 
+##################convolution and gaussian filtering##################
+def convolution(image, kernel, average=False, verbose=False):
+    if len(image.shape) == 3:
+        print("Found 3 Channels : {}".format(image.shape))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        print("Converted to Gray Channel. Size : {}".format(image.shape))
+    else:
+        print("Image Shape : {}".format(image.shape))
+ 
+    print("Kernel Shape : {}".format(kernel.shape))
+ 
+    if verbose:
+        plt.imshow(image, cmap='gray')
+        plt.title("Image")
+        plt.show()
+ 
+    image_row, image_col = image.shape
+    kernel_row, kernel_col = kernel.shape
+ 
+    output = np.zeros(image.shape)
+ 
+    pad_height = int((kernel_row - 1) / 2)
+    pad_width = int((kernel_col - 1) / 2)
+ 
+    padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
+ 
+    padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
+ 
+    if verbose:
+        plt.imshow(padded_image, cmap='gray')
+        plt.title("Padded Image")
+        plt.show()
+ 
+    for row in range(image_row):
+        for col in range(image_col):
+            output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
+            if average:
+                output[row, col] /= kernel.shape[0] * kernel.shape[1]
+ 
+    print("Output Image size : {}".format(output.shape))
+ 
+    if verbose:
+        plt.imshow(output, cmap='gray')
+        plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
+        plt.show()
+ 
+    return output
 
+    
+def dnorm(x, mu, sd):
+    return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
+ 
+
+def gaussian_kernel(size, sigma=1, verbose=False):
+ 
+    kernel_1D = np.linspace(-(size // 2), size // 2, size)
+    for i in range(size):
+        kernel_1D[i] = dnorm(kernel_1D[i], 0, sigma)
+    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
+ 
+    kernel_2D *= 1.0 / kernel_2D.max()
+ 
+    if verbose:
+        plt.imshow(kernel_2D, interpolation='none',cmap='gray')
+        plt.title("Image")
+        plt.show()
+ 
+    return kernel_2D
+
+def gaussian_blur(image, kernel_size, verbose=False):
+    kernel = gaussian_kernel(kernel_size, sigma=math.sqrt(kernel_size), verbose=verbose)
+    return convolution(image, kernel, average=True, verbose=verbose)
+ 
+####################################################################
 
 
 
@@ -365,9 +441,9 @@ image1 = threshold(dilation_result, thresh)
 erosion_result = erosion(image1, k, 1)
 image = threshold(erosion_result, thresh)
 
-# result = connected_component_labelling(image)
-# print_image(result)
-
+cv.imshow('Thresholded', img)
+cv.imshow('Dilation', image1)
+cv.imshow('Erosion', image)
 
 imgs = binarize(image, thresh)
 img = connected_component_labelling(imgs)
@@ -375,11 +451,8 @@ img = connected_component_labelling(imgs)
 coloured_img = colourize(img)
 coloured_img.show()
 
-
-cv.imshow('Thresholded', img)
-cv.imshow('Dilation', image1)
-cv.imshow('Erosion', image)
-
+imggaus = gaussian_blur(img, 5, verbose=True)
+cv.imshow("Gaussian: ", imggaus)
 
 print("Threshold = ", thresh)
 
